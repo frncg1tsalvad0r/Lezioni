8bf58edf32ce5c5a4ffff20d481e2ed17fad6121
frncg1tsalvad0r <132061377+frncg1tsalvad0r@users.noreply.github.com>
2024-12-16 19:38:04 +0100

    Modificato il commento di conosoleTenTimes

--- Esempi EspruinoESP32DevkitCV4/ConsolteTenTimes.js
+++ Esempi EspruinoESP32DevkitCV4/ConsolteTenTimes.js
@@ -1,5 +1,7 @@
 // QUI INSERIRETE IL PROGRAMMA DA SCARICARE SULLA BOARD ESP32Devkit32C
 
+// Questo programma semplice scrive sulla console Ciao a tutti i volete.
+
 for(var i = 0; i < 10; i++) {
   console.log('Ciao a tutti ' + i + ' volte');
 }
